e62cac01d41e53550cb674a18e70c028c2d154ef
Christian Jordan <gcp547@alumni.ku.dk>
2024-09-09 16:27:14 +0200

    added comments for fib.s

--- lectures/24-09-09_functions_and_text/exercise_solutions/fib.s
+++ lectures/24-09-09_functions_and_text/exercise_solutions/fib.s
@@ -1,28 +1,42 @@
 init:
+        # fib(7)
         li a0, 7
         jal ra, fib
+
+        # Exit
         jal zero, end
 
 fib:
-        addi sp, sp, -12
-        sw ra, 8(sp)
-        sw a0, 4(sp)
+        # Callee saves stack variables 
+        addi sp, sp, -12        # Prepare enough stack space for 3 variables * 4 bytes 
+        sw ra, 8(sp)            # Save return address on the stack
+        sw a0, 4(sp)            # Save function argument on the stack
+        
+        # if (a0 < 3) { return 1; }
         li t0, 3
         blt a0, t0, base
+
+        # int x = fib(a0 - 1)
         addi a0, a0, -1
         jal ra, fib
-        sw a0, 0(sp)
+        sw a0, 0(sp) 
         lw a0, 4(sp)
+        
+        # int y = fib(a0 - 1)
         addi a0, a0, -2
         jal ra, fib
         lw t0, 0(sp)
+        
+        # return x + y
         add a0, t0, a0
         jal zero, return
 base:
+        # Base case: if (a0 < 3)
         li a0, 1
 return:
-        lw ra, 8(sp)
-        addi sp, sp, 12
-        jalr zero, ra, 0
+        # Restore saved stack variables
+        lw ra, 8(sp)            # Load stored return address
+        addi sp, sp, 12         # Remove stack space for 3 variables * 4 bytes
+        jalr zero, ra, 0        # Return to caller
 
 end:
